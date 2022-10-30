39529e5912e7e1e4a0d4800627669c3906f2d194
Sergey Zaichkov <sergey.zaichkov@yandex.ru>
2022-10-30 13:36:46 +0100

    Initial Commit

---
+++ TrafficLight/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLight
+//
+//  Created by Sergey on 30.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
